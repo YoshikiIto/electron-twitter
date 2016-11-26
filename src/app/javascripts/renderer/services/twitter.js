9d48e47d54c69902036cdbec3da75d088799b124
YoshikiIto <yoshikiito.el@gmail.com>
2016-11-26 23:35:40 +0900

    main.jsx
    Timelineクラスを実装する、まで。

---
+++ src/app/javascripts/renderer/services/twitter.js
@@ -0,0 +1,8 @@
+const Twit = require('twit');
+
+module.exports = new Twit({
+	consumer_key: 'ioJMcE0mztyFJnuMQSFV5EKwe',
+	consumer_secret: 'QS0ZKXcQxcGQ3PE0Hg91cVKv3RzMpDWacpntlYMnjyWr9qvaqs',
+	access_token: '282599251-UoVARreoEVuqCiTWX1fGbIcxWy2JvDg8ayXMU1RE',
+	access_token_secret: 'Any08DRGnMhS4MB7JsLLsvNqiUGOxB0G1sE6VGBMfj6qR'
+})
